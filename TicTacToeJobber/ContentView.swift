15db69b0d53aeaa6a180aa5449de8087b23c5637
Calvin Aquino <calving.aquino@gmail.com>
2023-09-30 16:17:25 -0700

    Projects first commit

---
+++ TicTacToeJobber/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TicTacToeJobber
+//
+//  Created by Calvin Gonçalves de Aquino on 2023-09-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TicTacToeJobber/TicTacToeJobberApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeJobberApp.swift
+//  TicTacToeJobber
+//
+//  Created by Calvin Gonçalves de Aquino on 2023-09-30.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeJobberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
