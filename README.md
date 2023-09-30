d4ccb08d21c728bbdd97a1b3b0a2b1f7ce166d94
Calvin Aquino <calving.aquino@gmail.com>
2023-09-30 16:32:30 -0700

    Update Readme - Add basic board rendering just to have something visible - Limit app to portrait orientation for now

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # TicTacToeJobber
+
+A basic TicTacToe App with leaderboard.
+
+## Dependencies
+- iOS 16
+
+## Technologies
+- SwiftUI
+
+## TODO:
+- [ ] board state
+- [ ] marking
+- [ ] leaderboard
+- [ ] reseting the board
+- [ ] Persist between app sessions
--- TicTacToeJobber/ContentView.swift
+++ TicTacToeJobber/ContentView.swift
@@ -1,21 +1,20 @@
-//
-//  ContentView.swift
-//  TicTacToeJobber
-//
-//  Created by Calvin Gonçalves de Aquino on 2023-09-30.
-//
-
 import SwiftUI
+
+private let columns: [GridItem] = [
+    GridItem(.flexible()),
+    GridItem(.flexible()),
+    GridItem(.flexible())
+]
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        LazyVGrid(columns: columns) {
+            ForEach(0..<9) { index in
+                Image(systemName: index % 2 == 1 ? "xmark" : "circle")
+                    .frame(width: 100, height: 100)
+                    .background(Color.red)
+            }
         }
-        .padding()
     }
 }
 
