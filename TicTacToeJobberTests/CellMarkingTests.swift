e6822056e858d2a6fecc8696ad6718569ae797cb
Calvin Aquino <calving.aquino@gmail.com>
2023-09-30 17:42:53 -0700

    Add more victory tests when marking cells

--- TicTacToeJobberTests/CellMarkingTests.swift
+++ TicTacToeJobberTests/CellMarkingTests.swift
@@ -25,4 +25,29 @@ final class CellMarkingTests: XCTestCase {
         game.markCell(at: 3)
         XCTAssertNotEqual(game.mark(for: 3), .cirle)
     }
+    
+    func testCellMarkingChecksVictoryPatterns() {
+        let game = GameViewModel()
+        game.boardState = [
+            1,1,0,
+            0,2,0,
+            2,0,0
+        ]
+        game.markCell(at: 2)
+        XCTAssertEqual(game.mark(for: 2), .cross)
+        XCTAssertEqual(game.winner, .cross)
+    }
+    
+    func testCellMarkingEndsGameIfBoardIsFull() {
+        let game = GameViewModel()
+        game.boardState = [
+            1,2,0,
+            2,2,1,
+            2,1,2
+        ]
+        game.markCell(at: 2)
+        XCTAssertEqual(game.mark(for: 2), .cross)
+        XCTAssertNil(game.winner)
+        XCTAssertNil(game.currentPlayer)
+    }
 }
